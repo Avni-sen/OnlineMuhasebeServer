ba91e5b8364a0f9511d8d8512ee860ac5a6bd24f
Avni Kaan ŞEN <avnikaan.sen@realkom.com>
2023-01-25 23:53:48 +0300

    Add project files.

---
+++ OnlineMuhasebeServer.Presentation/AssemblyReference.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace OnlineMuhasebeServer.Presentation;
+
+public static class AssemblyReference
+{
+    //oluşturma sebebimiz program.cs te controller ı başka bir katmana taşımak istersen o katmana assemblsini vermemiz lazım.
+    public static readonly Assembly Assembly = typeof(Assembly).Assembly;
+}
